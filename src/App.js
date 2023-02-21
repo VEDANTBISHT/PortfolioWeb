56b78006ae17bea8b63e46f065cdd92f1519e6e7
Vedant <vedantbisht111@gmail.com>
2023-02-21 23:54:03 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import Services from './Components/services/Services';
+import About from './Components/about/About';
+import Resume from './Components/resume/Resume';
+import Portfolio from './Components/portfolio/Portfolio';
+import Contact from './Components/contact/Contact';
+import Testimonials from './Components/testimonials/Testimonials';
+import Blog from './Components/blog/Blog';
+import Pricing from './Components/pricing/Pricing';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <>
+    <Sidebar />
+    <main className='main'>
+      <Home />
+      <Services />
+      <About />
+      <Resume />
+      <Portfolio />
+      <Contact />
+      <Testimonials />
+      <Blog />
+      <Pricing />
+      
+    </main>
+    </>
+  )
 }
+
 
 export default App;
---
+++ src/Components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./about.css";
+const Home = () =>{
+    return(
+        <div>About</div>
+    )
+}
+
+
+export default About
+
