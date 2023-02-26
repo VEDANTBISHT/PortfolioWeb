656a956cd9d72fc52201fe28feaf754cb2eb2d15
Vedant <vedantbisht111@gmail.com>
2023-02-26 18:55:08 +0530

    sidebar jsx

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -1,8 +1,63 @@
 import React from 'react';
 import "./sidebar.css";
+import logo from "../../assets/logo.svg";
 const Sidebar = () =>{
     return(
-        <div>Sidebar</div>
+        <aside classname='aside'>
+            <a href="#home" classname="nav__logo">
+                <img src={Logo} alt="logo" />
+            </a>
+            <nav className="nav">
+                <div className='nav__menu'>
+                    <ul className='nav__list'>
+                        <li className='nav__item'>
+                            <a href="#home" className="nav__link">Home</a>
+                            <i className="icon-home"></i>
+                        </li>
+
+
+                        <li className='nav__item'>
+                            <a href="#about" className="nav__link">Home</a>
+                            <i className="icon-user-following"></i>
+                        </li>
+
+
+                        <li className='nav__item'>
+                            <a href="#services" className="nav__link">Home</a>
+                            <i className="icon-briefcase"></i>
+                        </li>
+
+
+                        <li className='nav__item'>
+                            <a href="#resume" className="nav__link">Home</a>
+                            <i className="icon-graduation"></i>
+                        </li>
+
+
+                        <li className='nav__item'>
+                            <a href="#portfolio" className="nav__link">Home</a>
+                            <i className="icon-layers"></i>
+                        </li>
+
+
+                        <li className='nav__item'>
+                            <a href="#blog" className="nav__link">Home</a>
+                            <i className="icon-note"></i>
+                        </li>
+
+
+                        <li className='nav__item'>
+                            <a href="#contact" className="nav__link">Home</a>
+                            <i className="icon-bubble"></i>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
+
+
+        </aside>
     )
 }
 
