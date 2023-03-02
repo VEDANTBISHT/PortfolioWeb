4b1762fdf524d400f6f26a4d24e9117c3a2daaed
Vedant <vedantbisht111@gmail.com>
2023-03-02 23:49:51 +0530

    home.jsx

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import "./home.css";
 const Home = () =>{
     return(
-        <div>Home</div>
+        <section className="home container" id="home">
+            <div className="intro">
+            
+            </div>
+
+
+        </section>
     )
 }
 
