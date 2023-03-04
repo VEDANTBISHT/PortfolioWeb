0240d01327228427e5d2d8728954d05914623c3f
Vedant <vedantbisht111@gmail.com>
2023-03-04 21:43:34 +0530

    Home.jsx updated

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import "./home.css";
+import Me from "..//..//assets/avatar-1.svg";
 const Home = () =>{
     return(
         <section className="home container" id="home">
             <div className="intro">
+                <img src="" alt="" className="home__img"/>
+                <h1 className="home_name">Vedant Bisht</h1>
+                <span className="home__education">I' am a Full-Stack-Developer</span>
             
             </div>
 
