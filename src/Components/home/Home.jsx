8c8cf5d1b7b80c5afcaaf384c28436f67b08a3bc
Vedant <vedantbisht111@gmail.com>
2023-03-06 21:35:19 +0530

    committed home.jsx

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -3,6 +3,7 @@ import "./home.css";
 // eslint-disable-next-line
 import ME from "..//..//assets/avatar-1.svg";
 import HeaderSocials from './Header.Socials';
+import ScrollDown from './ScrollDown';
 const Home = () =>{
     return(
         <section className="home container" id="home">
@@ -11,15 +12,18 @@ const Home = () =>{
                 <h1 className="home_name">Vedant Bisht</h1>
                 <span className="home__education">I' am a Full-Stack-Developer
                 </span>
-                
+
                 <HeaderSocials/>
 
+                <a href="#contact" className="btn">Hire Me</a>
+
+                <ScrollDown/>
 
             </div>
 
 
         </section>
-    )
-}
+    );
+};
 
 export default Home
