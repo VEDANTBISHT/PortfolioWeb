75811350a1db0fcdfb96239bc49187afc23171f6
Vedant <vedantbisht111@gmail.com>
2023-03-06 21:05:48 +0530

    HS updated

--- src/Components/home/Header.Socials.jsx
+++ src/Components/home/Header.Socials.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 const HeaderSocials = () =>{
     return(
         <div className="home__socials">
-            <a href="" className="home__social-link" target="_blank"></a>
+            <a href="" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+            </a>
+
+            <a href="" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-twitter"></i>
+            </a>
+
+            <a href="" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-behance"></i>
+            </a>
+
+            <a href="" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-dribbble"></i>
+            </a>
+
+            <a href="" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-printrest"></i>
+            </a>
+            
         </div>
     )
 }
