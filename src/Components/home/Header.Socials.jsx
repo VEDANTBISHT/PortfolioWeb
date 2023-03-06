2a398b7da22fecab5e3893e752f42b6dd20593ff
Vedant <vedantbisht111@gmail.com>
2023-03-06 21:15:22 +0530

    socials updated

--- src/Components/home/Header.Socials.jsx
+++ src/Components/home/Header.Socials.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 const HeaderSocials = () =>{
     return(
         <div className="home__socials">
-            <a href="" className="home__social-link" target="_blank">
+            <a href="https://www.instagram.com/" className="home__social-link" target="_blank">
             <i class="fa-brands fa-instagram"></i>
             </a>
 
-            <a href="" className="home__social-link" target="_blank">
+            <a href="https://www.twitter.com/" className="home__social-link" target="_blank">
             <i class="fa-brands fa-twitter"></i>
             </a>
 
-            <a href="" className="home__social-link" target="_blank">
+            <a href="https://www.behance.com/" className="home__social-link" target="_blank">
             <i class="fa-brands fa-behance"></i>
             </a>
 
-            <a href="" className="home__social-link" target="_blank">
+            <a href="https://www.dribble.com/" className="home__social-link" target="_blank">
             <i class="fa-brands fa-dribbble"></i>
             </a>
 
-            <a href="" className="home__social-link" target="_blank">
-            <i class="fa-brands fa-printrest"></i>
+            <a href="https://www.pinterest.com/" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-pinterest"></i>
             </a>
             
         </div>
