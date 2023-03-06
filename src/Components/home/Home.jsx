e32a8dda759a449d26cf2b3caa1633fa0c6449ea
Vedant <vedantbisht111@gmail.com>
2023-03-06 21:14:24 +0530

    socials updated

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./home.css";
 // eslint-disable-next-line
 import ME from "..//..//assets/avatar-1.svg";
+import HeaderSocials from './Header.Socials';
 const Home = () =>{
     return(
         <section className="home container" id="home">
@@ -10,6 +11,10 @@ const Home = () =>{
                 <h1 className="home_name">Vedant Bisht</h1>
                 <span className="home__education">I' am a Full-Stack-Developer
                 </span>
+                
+                <HeaderSocials/>
+
+
             </div>
 
 
